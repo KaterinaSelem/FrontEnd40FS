30419a2b4ed55070346b7c537bab2dad18a3a065
KaterinaS. <153716458+KaterinaSelem@users.noreply.github.com>
2024-05-29 23:22:05 +0200

    new task

--- 03_react_props_27_05_2024/task.js
+++ 03_react_props_27_05_2024/task.js
@@ -1,46 +1,61 @@
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const Task = ({title}) => {
-  // const isEdit = true;
-  const [isEdit, setIsEdit] = React.useState(false); //[isEdit, setIsEdit] Array(2) изначально из эдит равно фолс
+const Task = ({ title, deleteTask }) => {
+  // const isEdit = false;
+  const [isEdit, setIsEdit] = React.useState(false); 
 
   const [updatedTask, setUpdatedTask] = React.useState(title);
 
-const textId = React.useRef();
+  // 1. Получение абстрактной ссылки, которую впоследствии можно повесить на любой из элементов
+  const textId = React.useRef();
+
+  const handleClickSave = () => {
+      setUpdatedTask(textId.current.value);
+      setIsEdit(false);
+  }
 
   if (isEdit) {
-    return (
-      <>
-        <textarea defaultValue=''></textarea>
-        <button onClick = {()=>setIsEdit(false)}>Save</button>
-      </>
-    );
+      return (
+          <>
+              {/* 2. Соединяем абстрактную ссылку с конкретным элементом, на который данная ссылка будет вести */}
+              <textarea ref={textId} defaultValue={updatedTask}></textarea>
+              {/* () => setIsEdit(false)     setIsEdit(false)        setIsEdit*/}
+              <button onClick={handleClickSave}>Save</button>
+          </>
+      )
   } else {
-    return (
-      <>
-        <p>Name</p>
-        <button onClick = {()=>setIsEdit(true)}>Edit</button>
-        <button>Delete</button>
-      </>
-    );
+      return (
+          <>
+              <p>{updatedTask}</p>
+              <button onClick={() => setIsEdit(true)}>Edit</button>
+              <button onClick={deleteTask}>Delete</button>
+          </>
+      )
   }
-};
+}
 
 const TaskList = () => {
-    const [tasks, setTasks] = React.useState(['Task1', 'Task2', 'Task3']);
-            
-    return (
-        <>
-        {tasks.map((task, index) => ( <Task
-            key = {index}
-            title = {task}
-            />
-        ))
-        }
-        </>
-    )
+  const [tasks, setTasks] = React.useState([{ id: 1, title: 'Task 1' },
+  { id: 2, title: 'Task 2' },
+  { id: 3, title: 'Task 3' }]);
+  
+  const deleteTask = (taskId) => {
+    setTasks((tasks) => tasks.filter((task) => task.id !== taskId));
+  }
+  
+  return (
+      <>
+          { tasks.map((task, i) => (
+              <Task
+                  key={task.id}
+                  title={task.title}
+                  deleteTask={() => deleteTask(task.id)}
+              />
+          )) }
+      </>
+  )
 }
 
 root.render(
-<TaskList />
-);
+  <TaskList />
+)
